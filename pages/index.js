51e03168b6c644e70a74fdada9b0928aa8cf7a47
Awais Ahmad <awais.ahmad1990@yahoo.com>
2021-02-13 15:28:37 +0500

    Update api url for testing

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ const Home = ({data}) => {
 )}
 
 export async function getStaticProps(context) {
-  const res = await fetch("http://localhost:3000/api/daily");
+  const res = await fetch("https://mct-mauve.vercel.app/api/daily");
   const json = await res.json();
   return {
     props: {
